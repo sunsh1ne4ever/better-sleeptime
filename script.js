c4e3cff49c87047d006a9877412ab2ff1b86a23d
Emma Levitsky <eecompcode@mail.com>
2022-09-06 10:23:18 -0400

    Changed Javascript to reflect chosen time

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const colors = [
-  "white",
-  "red",
-  "white",
-  "red",
-  "white",
-  "red",
+  "rgb(168, 39, 254)",
+  "rgb(154, 3, 254)",
+  "rgb(150, 105, 254)",
+  "rgb(140, 140, 255)",
+  "rgb(187, 187, 255)",
+  "rgb(143, 254, 221)",
 ]
 function handleOnClick() {
     let output = document.querySelector(".output");
@@ -12,11 +12,15 @@ function handleOnClick() {
   
     let hours = document.getElementById("hours");
     hours.innerHTML = "";
+
+    let hh = document.getElementById("hh").value;
+    let mm = document.getElementById("mm").value;
+    let amppm = document.getElementById("ampm").value;
   
     let now = new Date();
-    // allow 14 minutes to fall sleep!
-    now.setMinutes(now.getMinutes() + 14);
-  
+    now.setHours(hh);
+    now.setMinutes(mm);
+
     // calculate sleep cycles!
     for (let i = 1; i <= 6; i++) {
       now.setMinutes(now.getMinutes() + 90);
